bd3f2cd1df1ba05f2a10b415e5c2223e62d0de89
Camila Solér <msoler.camila@gmail.com>
2022-06-04 17:03:32 -0300

    help from Flavia and Pedro on getting the data from sephora api

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,34 +1,192 @@
 const router = require("express").Router();
+const axios = require("axios");
 
 const ProductModel = require("../models/Product.model");
 const ReviewModel = require("../models/Review.model");
 const UserModel = require("../models/User.model");
 
 // Rota de produtos sem auth
-router.post("/product", async (req, res) => {
-  try {
-    const data = req.body;
-    const result = await ProductModel.create({ ...data });
+// // router.post("/product", async (req, res) => {
+// //   try {
+// //     const data = req.body;
+// //     const result = await ProductModel.create({ ...data });
 
-    return res.status(201).json(result);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ msg: "Internal server error." });
+// //     return res.status(201).json(result);
+// //   } catch (err) {
+// //     console.error(err);
+// //     return res.status(500).json({ msg: "Internal server error." });
+// //   }
+// //
+// });
+
+// pegar as infos das rotas da api da sephora
+//acessar
+
+// function getProductsSearchList() {
+//   const result = await axios.get("https://sephora.p.rapidapi.com/products/");
+// };
+// console.log( getProductsSearchList();)
+
+// const objGetProductsSearchListSephoraApi = {
+//   method: "GET",
+//   url: "https://sephora.p.rapidapi.com/products/search",
+//   params: { q: "skincare", pageSize: "60", currentPage: "1" },
+//   headers: {
+//     "X-RapidAPI-Host": "sephora.p.rapidapi.com",
+//     "X-RapidAPI-Key": "e116d9383bmshc833e5efafe1044p1ce658jsn59781957b0e7",
+//   },
+// };
+
+// axios
+//   .request(objGetProductsSearchListSephoraApi)
+//   .then(function (response) {
+//     console.log("AQUI", response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
+
+let productsCategories = ["toner", "vitamin C"];
+
+async function exec(category) {
+  const categoryResList = await connectSearchApiSephora(category);
+
+  for (categoryRes of categoryResList) {
+    const categoryResDetails = await connectDetailsApiSephora(
+      categoryRes.productId_sephora,
+      categoryRes.preferedSku
+    );
+    let RESPOSTA = mapper_details(categoryRes, categoryResDetails);
+    console.log("HOLA QUE TAL", RESPOSTA);
   }
-});
+}
 
-router.get("/product/:_id", async (req, res) => {
-  try {
-    const { _id } = req.params;
-
-    const result = await ProductModel.findOne({ _id });
-
-    return res.status(200).json(result);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ msg: "Internal server error." });
+async function init() {
+  for (let category of productsCategories) {
+    await exec(category);
   }
-});
+}
+
+async function connectSearchApiSephora(searchParam) {
+  try {
+    const response = await axios.request({
+      method: "GET",
+      url: "https://sephora.p.rapidapi.com/products/search",
+      params: { q: searchParam, pageSize: "5", currentPage: "1" },
+      headers: {
+        "X-RapidAPI-Host": "sephora.p.rapidapi.com",
+        "X-RapidAPI-Key": "e116d9383bmshc833e5efafe1044p1ce658jsn59781957b0e7",
+      },
+    });
+    let productList = [...response.data.products];
+    return productList.map((product) =>
+      mapper_search_allTypes_products(product)
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function connectDetailsApiSephora(productId, preferedSku) {
+  return axios
+    .request({
+      method: "GET",
+      url: "https://sephora.p.rapidapi.com/products/detail",
+      params: { productId: productId, preferedSku: preferedSku },
+      headers: {
+        "X-RapidAPI-Host": "sephora.p.rapidapi.com",
+        "X-RapidAPI-Key": "e116d9383bmshc833e5efafe1044p1ce658jsn59781957b0e7",
+      },
+    })
+    .then(function (response) {
+      // console.log("AQUI", response.data);
+      return response.data;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
+// const toner = connectApiSephora("Toner");
+// const moisturizing = connectApiSephora("moisturizing");
+
+// SUGESTÃO => arquivo separado os mappers
+function getCategoryTranslation(batata) {
+  if (batata.toLowerCase().indexOf("peel") > -1) {
+    return "peel";
+  } else if (batata.toLowerCase().indexOf("soap") > -1) {
+    return "soap";
+  } else if (batata.toLowerCase().indexOf("moisture") > -1) {
+    return "moisture";
+  } else {
+    return batata;
+  }
+}
+
+function mapper_search_allTypes_products(obj_search) {
+  return {
+    productName: obj_search.displayName,
+    imageDetails: obj_search.image450,
+    imageIcon: obj_search.image135,
+    shortDescription: "",
+    longDescription: "",
+    brandName: obj_search.brandName,
+    howToUse: "",
+    ingredients: "",
+    rating: Number(Number(obj_search.rating).toFixed(2)),
+    averagePrice: obj_search.currentSku.listPrice,
+    productSkinType: "oil",
+    category: getCategoryTranslation(obj_search.displayName),
+    productId_sephora: obj_search.productId,
+    preferedSku: obj_search.currentSku.skuId,
+  };
+}
+
+function mapper_details(obj_search, obj_details) {
+  return {
+    ...obj_search,
+    shortDescription: obj_details.shortDescription,
+    longDescription: obj_details.longDescription,
+    howToUse: obj_details.suggestedUsage,
+    ingredients: obj_details.currentSku.ingredientDesc,
+  };
+}
+
+init();
+// router.get("/product/search", async (req, res) => {
+//   try {
+//     const result = await ProductModel.findOne({ _id });
+
+//     return res.status(200).json(result);
+//   } catch (err) {
+//     console.error(err);
+//     return res.status(500).json({ msg: "Internal server error." });
+//   }
+// });
+
+// router.get("/product/", async (req, res) => {
+//   try {
+//     const result = await ProductModel.find({ _id }); //
+
+//     return res.status(200).json(result);
+//   } catch (err) {
+//     console.error(err);
+//     return res.status(500).json({ msg: "Internal server error." });
+//   }
+// });
+// //   try {
+// //
+
+// const { _id } = req.params;
+
+//     const result = await ProductModel.findOne({ _id });
+
+//     return res.status(200).json(result);
+//   } catch (err) {
+//     console.error(err);
+//     return res.status(500).json({ msg: "Internal server error." });
+//   }
+// });
 
 // // Rota de produtos favoritos
 // router.post("/product", isAuthenticated, async (req, res) => {
